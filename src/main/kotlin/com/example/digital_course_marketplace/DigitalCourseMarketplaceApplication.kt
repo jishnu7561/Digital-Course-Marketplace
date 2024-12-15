4bffe7ea9688e760c37f9876fe82b571b97dd1e8
jishnu7561 <jishnujish0838@gmail.com>
2024-12-15 12:14:09 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/digital_course_marketplace/DigitalCourseMarketplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.digital_course_marketplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DigitalCourseMarketplaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DigitalCourseMarketplaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/digital_course_marketplace/DigitalCourseMarketplaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.digital_course_marketplace
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DigitalCourseMarketplaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
